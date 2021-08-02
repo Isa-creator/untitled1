96152870b8437dc3b707e20788576ec533615ae4
Isa-creator <isakalybekov@gmail.com>
2021-08-02 22:51:57 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// домашная задание
+package com.company;
+
+        public class Main {
+
+            public static void main(String[] args) {
+                // write your code here
+
+                String legoBishkek; //created
+                final int NUM = 0; //constant , created, initialilized
+                String word = " something";
+                //float b = 0.5f;
+                //boolean v = false;
+                //char c = '+';
+
+                legoBishkek = NUM + word;
+                System.out.println(legoBishkek);
+                if (NUM < 0) {
+                    System.out.println("вы сохранили отрицательное число ");
+                } else if (NUM > 0) {
+                    System.out.println("вы сохранили положительный число ");
+                }else {
+                    System.out.println("вы сохранили ноль ");
+                }
+
+
+            }
+        }
+    }
+}
